1f768636ff22561696c9977b441b92221d472486
kwokh434 <91524518+kwokh434@users.noreply.github.com>
2022-05-18 08:12:05 -0400

    Add files via upload
    on punctuation

---
+++ poem/pun1.js
@@ -0,0 +1,217 @@
+function myFunction() {
+  var x = document.getElementById("1");
+  x.textContent = "There are some punctuations that are interesting and there are some punctuations that are not. Let us begin with the punctuations that are not. Of these the one but the first and the most the completely most uninteresting is the question mark.";
+}
+
+function myFunction2(){
+     var x2 = document.getElementById("2");
+  x2.textContent = "The question mark is alright when it is all alone when it is used as a brand on cattle or when it could be used in decoration but connected with writing it is completely entirely completely uninteresting. It is evident that is you ask a question you ask a question but anybody who can read at all knows when a question is a question as it is written in writing."
+}
+
+function myFunction3(){
+     var x2 = document.getElementById("3");
+  x2.textContent = "Therefore I ask you therefore wherefore should one use the question mark."
+}
+
+function myFunction4(){
+     var x2 = document.getElementById("4");
+  x2.textContent = "Beside it does not in its form go with ordinary printing and so it pleases neither the eye nor the ear and it is therefore like a noun, just an unnecessary name of something. A question is a question, anybody can know that a question is a question and so why add to it the question mark when it is already there when the question is already there in the writing. Therefore I never could bring myself to use a question mark, I always found it positively revolting, and now very few do use it."
+}
+
+function myFunction5(){
+     var x2 = document.getElementById("5");
+  x2.textContent = "Exclamation marks have the same difficulty and also quotation marks, they are unnecessary, they are ugly, they spoil the line of the writing or the printing and anyway what is the use, if you do not know that a question is a question what is the use of its being a question."
+}
+
+function myFunction6(){
+     var x2 = document.getElementById("6");
+  x2.textContent = " The same thing is true of a quotation. When I first began writing I found it simply impossible to use question marks and quotation marks and exclamation points and now anybody sees it that way. Perhaps some day they will see it some other way but not at any rate anybody can and does see it that way."
+}
+
+function myFunction7(){
+     var x2 = document.getElementById("7");
+  x2.textContent = "So there are the uninteresting things in punctuation uninteresting in a way that is perfectly obvious, and so we do not have to go any farther into that. There are besides dashes and dots, and these might be interesting spaces might be interesting. They might if one felt that way about them."
+}
+
+function myFunction8(){
+     var x2 = document.getElementById("8");
+  x2.textContent = "One other little punctuation mark one can have feelings about and that is the apostrophe for possession."
+}
+
+function myFunction9(){
+     var x2 = document.getElementById("9");
+  x2.textContent = "Well feel as you like about that, I can see and I do see that for many that for some the possessive case apostrophe has a gentle tender insinuation that makes it very difficult to definitely decide to do without it. One does do without it, I do, I mostly always do, but I cannot deny that from time to time I feel myself having regrets and from time to time I put it in to make the possessive case. I absolutely do not like it and leaving it out I feel no regret, there it is unnecessary and not ornamental but inside a word and its s well perhaps, perhaps it does appeal by its weakness to your weakness. At least at any rate from time to time I do find myself letting it alone if it has come in and sometimes it has come in. I cannot positively deny but that I do from time to time let it come in."
+}
+
+function myFunction10(){
+     var x2 = document.getElementById("10");
+  x2.textContent = "So now to come to the real question of punctuation, periods, commas, colons, semi-colons and capitals and small letters.I have had a long and complicated life with all these."
+}
+
+function myFunction11(){
+     var x2 = document.getElementById("11");
+  x2.textContent = "Let us begin with these I use the least first and these are colons and semi-colons, one might add to these commas."
+}
+
+function myFunction12(){
+     var x2 = document.getElementById("12");
+  x2.textContent = "When I first began writing, I felt that writing should go on, I still do feel that it should go on but when I first began writing I was completely possessed by the necessity that writing should go on and on and if writing should go on what had colons and commas to do with it, what had periods to do with it what had small letters and capitals to do with it to do with writing going on which was at that time the most profound need I had in connection with writing."
+}
+
+function myFunction13(){
+     var x2 = document.getElementById("13");
+  x2.textContent = "What had colons and semi-colons to do with it what had commas to do with it what had periods to do with it."
+}
+
+function myFunction14(){
+     var x2 = document.getElementById("14");
+  x2.textContent = "What had periods to do with it."
+}
+
+function myFunction15(){
+     var x2 = document.getElementById("15");
+  x2.textContent = "Inevitably no matter how completely I had to have writing go on, physically one had to again and again stop sometime and if one had to again and again stop sometime and if one had to again and again stop some time then periods had to exist."
+}
+
+function myFunction16(){
+     var x2 = document.getElementById("16");
+  x2.textContent = "Beside I had always like the look of periods and I liked what they did. Stopping sometime did not really keep one from going on, it was nothing that interfered, it was only something that happened, and as it happened as a perfectly natural happening, I did not believe in periods and I used them. I really never stopped using them."
+}
+
+function myFunction17(){
+     var x2 = document.getElementById("17");
+  x2.textContent = "Beside that periods might later come to have a life of their own to commence breaking up things in arbitrary ways, that has happened lately with me in a poem I have written called Winning His Way, later I will read you a little of it. "
+}
+
+function myFunction18(){
+     var x2 = document.getElementById("18");
+  x2.textContent = " By the time I had written this poem about three years ago periods had come to have for me completely a life of their own. "
+}
+
+function myFunction19(){
+     var x2 = document.getElementById("19");
+  x2.textContent = "They could begin to act as they thought best and one might interrupt one’s writing with them that is not really interrupt one’s writing with them but one could come to stop arbitrarily stop at times in one’s writing and so they could be used and you could use them Periods could come to exist in this way and they could come in this way to have a life of their own."
+}
+
+function myFunction20(){
+     var x2 = document.getElementById("20");
+  x2.textContent = " They did not serve you in any servile way as commas and colons and semi-colons do. Yes you do feel what I mean."
+}
+
+function myFunction21(){
+     var x2 = document.getElementById("21");
+  x2.textContent = "Periods have a life of their own a necessity of their own a feeling of their own a time of their own."
+}
+
+function myFunction22(){
+     var x2 = document.getElementById("22");
+  x2.textContent = "And that feeling that life that necessity that time can express itself in an infinite variety that is the reason that I have always remained true to periods so much so that as I say recently I have felt that one could need them more than one had ever needed them."
+}
+
+function myFunction23(){
+     var x2 = document.getElementById("23");
+  x2.textContent = "You can see what an entirely different thing a period is from a comma, a colon or a semi-colon."
+
+}
+
+function myFunction24(){
+     var x2 = document.getElementById("24");
+  x2.textContent = "There are two different way of thinking about colons and semicolons."
+}
+
+function myFunction25(){
+     var x2 = document.getElementById("25");
+  x2.textContent = "You can think of them as commas and as such they are purely servile or you can think of them as periods and then using them can make you feel adventurous, I can see that one might feel about them as periods but I myself never have I began unfortunately to feel them as a comma and commas are servile they have no life of their own they are dependent upon use and convenience and they are put there just for practical purposes."
+}
+
+function myFunction26(){
+     var x2 = document.getElementById("26");
+  x2.textContent = " Semi-colons and colons had for me from the first completely this character the character that a comma has and not the character that a period has and therefore and definitely I have never used them."
+}
+
+function myFunction27(){
+     var x2 = document.getElementById("27");
+  x2.textContent = "But now dimly and definitely I do see that they might well possibly have in them something of the character of the period and so it might have been an adventure to use them. I really do not think so."
+}
+
+function myFunction28(){
+     var x2 = document.getElementById("28");
+  x2.textContent = "I think however lively they are or disguised they are, they are definitely more comma than period and so really I cannot regret not having used them."
+}
+
+function myFunction29(){
+     var x2 = document.getElementById("29");
+  x2.textContent = "They are more powerful, more imposing, more pretentious than a comma but they are a comma all the same."
+}
+function myFunction30(){
+     var x2 = document.getElementById("30");
+  x2.textContent = "They really have within them deeply within them fundamentally within them the comma nature."
+}
+function myFunction31(){
+     var x2 = document.getElementById("31");
+  x2.textContent = "And now what does a comma do and what has it to do and why do I feel as I do about them."
+}
+function myFunction32(){
+     var x2 = document.getElementById("32");
+  x2.textContent = "What does a comma do.I have refused them so often and left the out so much and did without them so continually that I have come finally to be indifferent to them. I do not now care whether you put them in or not but for a long time I felt very definitely about them and would have nothing to do with them."}
+function myFunction33(){
+     var x2 = document.getElementById("33");
+  x2.textContent = "As I say commas are servile and they have no life of their own, and their use is not a use, it is a way of replacing one’s own interest and I do decidedly like to like my own interest my own interest in what I am doing."
+}
+function myFunction34(){
+     var x2 = document.getElementById("34");
+  x2.textContent = "As I say commas are servile and they have no life of their own, and their use is not a use, it is a way of replacing one’s own interest and I do decidedly like to like my own interest my own interest in what I am doing."
+}
+
+
+
+
+const buttonlist = document.querySelectorAll("button");
+
+for(let i=0;i<buttonlist.length; i++){ 
+buttonlist[i].style.opacity='0%'
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+for(let i=0;i<buttonlist.length; i++){ 
+   
+   setTimeout(() => { 
+   
+   let margin=getRandomInt(300)
+  
+   buttonlist[i].style.marginLeft=margin +'px';
+   buttonlist[i].style.opacity='100%';
+    }, i*1000);
+
+}
+
+
+function setup() {
+  let canvas = createCanvas(1000, 1000);
+  canvas.id("myCanvas2");
+}
+
+function draw() {
+      // background('#fbf8f3')
+  if (mouseIsPressed) {
+    pen()
+  }
+}
+
+function pen() {
+  // set the color and weight of the stroke
+   
+  stroke(0, 0, 0, 255)
+  strokeWeight(2)
+  line(mouseX, mouseY, pmouseX, pmouseY)
+}
+
+
+
+  
+
+
+
+
