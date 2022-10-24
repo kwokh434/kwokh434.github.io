debcbe35401ff74402f70444aa2ef105640f8c00
kwokh434 <kwokh434@newschool.edu>
2022-10-24 15:01:39 -0400

    logo mockups setup

--- logo/logo.js
+++ logo/logo.js
@@ -9,40 +9,97 @@ function preload (){
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000,1000);
   textSize(fontSize);
   textFont(muoto);
-  background(220);
+  background('white');
   logoArray = muoto.textToPoints("Bloomberg",150,450,fontSize,{
-    sampleFactor: 0.2});
+    sampleFactor: 0.4});
 
-}
+}  
+function time0(){
+  background('white');
 
-function time0() {
+  for (let i=0; i < logoArray.length; i++){
+
+    push()
+    translate(logoArray[i].x,logoArray[i].y);
+
+    strokeWeight(1);
+    stroke('black');
+    line (-6,9,7,-7); // original
+    line (-5,5,9,9);   //orginal
+    pop()
+   }
+ }
+
+function time1() { //yellow
+  background('white');
+  for (let i=0; i < logoArray.length; i++){
+   
+    push()
+    translate(logoArray[i].x,logoArray[i].y);
+    strokeWeight(1);
+    stroke('#FEC41F');
+    line(-4,-4,9,9);            
+    line(-5,9,9,-5);            
+    pop()
+   }
+  }
+
+ function time2() {  //light green
+    background('white');
+
     for (let i=0; i < logoArray.length; i++){
+      push()
+    translate(logoArray[i].x,logoArray[i].y);
+    strokeWeight(1);
+    stroke('#0CE508');
+    line(4,8,-8,-4);
+    stroke('');
+    line(-6,-5,-12,5);
+    stroke('#11D82C');
+    line(-1,7,7,-1);
+    pop()
+   }
+  }
 
+ 
+   function time3() {  //dark green
+    background('white');
+    for (let i=0; i < logoArray.length; i++){
+  
       push()
       translate(logoArray[i].x,logoArray[i].y);
       strokeWeight(1);
-      line (6,6,5,-5);
+      stroke('#11D82C');
+      line(8,8,-4,-4);
+      stroke('#87EB46');
+      line(-3,5,-12,-5);
+      stroke('#55D900');
+      line(-8,7,7,-6);
       pop()
-}
+     }
+   }
 
-function time1() {
-  console.log('correct function call')
-  // text('Bloomberg',width/2,height/2);
-  // text(t1,140,270,100, 100);
-  // text(t2,180,270,100, 100);
-  // text(t3,213,263,100, 100);
-  
-  // push();
-  // translate(180,180);
-  // rotate(radians(4));
-  // text(t4,79,67,100, 100);
-  // pop();
+   function time4() {  //red
+    background('white');
 
-  // text(t5,330,270,100, 100);
-}
+    
+    for (let i=0; i < logoArray.length; i++){
+      push()
+      translate(logoArray[i].x,logoArray[i].y);
+      strokeWeight(1);
+      stroke('#DC0C02');
+      line(9,9,-9,-9);          
+      stroke('#');
+      line(9,7,-8,-9); 
+      stroke('#EF340A')
+      line(3,8,-3,4); 
+
+      pop()
+     }
+   }
 
 function draw(){
   
@@ -50,8 +107,15 @@ function draw(){
     time1() 
   } 
   else if (checktime === 2){
-    // time2()
+    time2()
   }
+  else if (checktime === 3){
+    time3()
+  }
+  else if (checktime === 4){
+    time4()
+  }
+
   //else if ...
   else {
     time0()
@@ -60,60 +124,51 @@ function draw(){
  
 }
 
-
+console.log('hello')
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'e9b54c99b8msh6f1169ef9422f5ep1d8739jsn6f40989b7069',
-		'X-RapidAPI-Host': 'world-clock.p.rapidapi.com'
+		'X-RapidAPI-Host': 'world-time2.p.rapidapi.com'
 	}
 };
 
-fetch(myUrl,options)
-.then(response => {
-  return response.json();
-})
-.then(data => {console.log(data.currentDateTime)
-  let time = data.currentDateTime.substring(data.currentDateTime.length - 11,data.currentDateTime.length - 6);
-   
+fetch('https://world-time2.p.rapidapi.com/ip', options)
+	.then(response => response.json())
+	.then (data => {console.log(data)
+
+// fetch(myUrl,options)
+// .then(response => {
+//   return response.json();
+// })
+// .then(data => {console.log(data.currentDateTime)
+  // let time = data.currentDateTime.substring(data.currentDateTime.length - 11,data.currentDateTime.length - 6);
+  let time = data.datetime.substring(data.datetime.length-16,data.datetime.length-21);
+
   console.log(time);
   //US stock market 
-  // if (time === '9:30'){
-  //  checktime = 1
-  // }
+  if (time === '9:30'){
+   checktime = 1
+  }
  
-  // else if (time === '11:30'){
-  //  checktime = 2
-  // }
+  else if (time === '11:30'){
+   checktime = 2
+  }
 
-  // else if( time === '15:00'){
-  //   checktime = 3
-  // }
+  else if( time === '13:00'){
+    checktime = 3
+  }
 
-  // else if( time === '16:00'){
-  //   checktime = 4
-  // }
+  else if( time === '15:00'){
+    checktime = 4
+  }
 
-  // //HK stock market 
-  // if (time === '21:30'){
-  //  checktime = 5
-  // }
- 
-  // else if (time === '23:30'){
-  //  checktime = 6
-  // }
-
-  // else if( time === '3:00'){
-  //   checktime = 7
-  // }
-
-  // else if( time === '4:00'){
-  //   checktime = 8
-  // }
-
+  else if( time === '16:00'){
+  checktime = 1
+  }
 })
-}
-// .catch(err => console.error(err));
+
+.catch(err => console.error(err));
 
 // function map(value, low1, high1, low2, high2) {
 //   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
